af8dd3f806ccd42974bdeefca1454b72219a8c09
Steven Zeiler <zeiler.steven@gmail.com>
2015-01-03 11:51:14 -0800

    [FOLD] Remove build/ directory from git

--- build/config/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function (router, controllers) {
-  router.get("/bridge_payments/:source/:source_currency/:destination/:destination_amount/:destination_currency", controllers.bridgeQuotes.create);
-
-  router.post("/bridge_payments/:source/:source_currency/:destination/:destination_amount/:destination_currency", controllers.bridgePayments.create);
-
-  router.get("/bridge_payments/:id", controllers.bridgePayments.show);
-};
