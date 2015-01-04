5e7617019c21f3f873097bf213efb642c06cf6fb
Steven Zeiler <zeiler.steven@gmail.com>
2015-01-03 17:54:58 -0800

    [FEATURE] Format gateway transaction.

--- src/controllers/bridge_payments.js
+++ src/controllers/bridge_payments.js
@@ -1,20 +1,35 @@
-module.exports = function(dependencies) {
-  var gatewayd = dependencies[0];
+module.exports = function(gatewayd) {
 
   return {
     create: function(req, res, next) {
-      res.status(200).send({
-        success     : true,
-        source      : {
-          address   : req.params.source,
-          currency  : req.params.source_currency
+
+      var gatewayTransactionParams = {
+        direction: 'from-ripple',
+        state    : 'invoice',
+        ripple: {
+          source_address       : req.params.source.split(':')[1],
+          source_currency      : req.params.source_currency,
+          source_amount        : req.params.destination_amount,
+          destination_currency : req.params.destination_currency,
+          destination_amount   : req.params.destination_amount
         },
-        destination : {
-          address   : req.params.destination,
-          currency  : req.params.destination_currency,
-          amount    : Number(req.params.destination_amount)
+        external: {
+          source_address       : 'coinbase:coin-gate.com',
+          source_amount        : Number(req.params.destination_amount),
+          source_currency      : req.params.destination_currency,
+          destination_address  : req.params.destination,
+          destination_amount   : Number(req.params.destination_amount),
+          destination_currency : req.params.destination_currency
         }
-      })
+      }
+      gatewayd.api.createGatewayTransaction(gatewayTransactionParams)
+        .then(function(gatewayTransaction) {
+          res.status(200).send({
+            success             : true,
+            gateway_transaction : gatewayTransaction
+          })
+        })
+        .error(next)
     },
 
     show: function(req, res, next) {
--- src/controllers/bridge_quotes.js
+++ src/controllers/bridge_quotes.js
@@ -1,17 +1,28 @@
 module.exports = function(gatewayd, models) {
   return {
     create: function(req, res, next) {
+      var gatewayTransactionParams = {
+        direction: 'from-ripple',
+        state    : 'invoice',
+        ripple: {
+          source_address       : req.params.source.split(':')[1],
+          source_currency      : req.params.source_currency,
+          source_amount        : req.params.destination_amount,
+          destination_currency : req.params.destination_currency,
+          destination_amount   : req.params.destination_amount
+        },
+        external: {
+          source_address       : 'coinbase:coin-gate.com',
+          source_amount        : Number(req.params.destination_amount),
+          source_currency      : req.params.destination_currency,
+          destination_address  : req.params.destination,
+          destination_amount   : Number(req.params.destination_amount),
+          destination_currency : req.params.destination_currency
+        }
+      }
       res.status(200).send({
         success     : true,
-        source      : {
-          address   : req.params.source,
-          currency  : req.params.source_currency
-        },
-        destination : {
-          address   : req.params.destination,
-          currency  : req.params.destination_currency,
-          amount    : Number(req.params.destination_amount)
-        }
+        quote       : gatewayTransactionParams
       })
     }
   }
